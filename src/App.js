3f260a234b2c82e5d93979682c1468ade264b162
AbigailToledo27 <magtoledo@udesa.edu.ar>
2026-09-11 14:30:41 -0300

    un poco mas de register, configuracion de router y swtich en app e index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import { Route } from 'react-router-dom/cjs/react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom';
 import './App.css';
 import CrearCuenta from './Screens/CrearCuenta/CrearCuenta';
 
 function App() {
   return (
     <div className="App">
-
+        <Switch>
+          <Route path="/register" component={CrearCuenta} />
+        </Switch>
     </div>
 
   );
--- src/Componentes/CrearCuentaForm/CrearCuentaForm.js
+++ src/Componentes/CrearCuentaForm/CrearCuentaForm.js
@@ -21,19 +21,29 @@ class CrearCuentaForm extends Component{
         })
     }
 
+    submit = (e) => {
+    e.preventDefault()
+
+    const crearUser = {
+        email: this.state.email,
+        password: this.state.password,
+        createdAt: Date.now() /*preguntaar si ponerlo como en la practica */
+    }
+    }
+    /*me faltan hacer las validaciones y guardar los datos con LocalStorage */
     render() {
         return(
             <>
                 <div class="row justify-content-center">
                     <div class="col-md-6">
-                        <form>
+                        <form onSubmit={this.submit}>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="Ingresá tu email"/>
+                                <input type="email" name="name" value={this.state.email} onChange={this.controlEmail} class="form-control" id="email" placeholder="Ingresá tu email"/>
                             </div>
                             <div class="form-group">
                                 <label for="password">Contraseña</label>
-                                <input type="password" class="form-control" id="password" placeholder="Ingresá tu contraseña"/>
+                                <input type="password" name="password" value={this.state.password} onChange={this.controlPass} class="form-control" id="password" placeholder="Ingresá tu contraseña"/>
                             </div>
                             <button type="submit" class="btn btn-primary btn-block">Registrarse</button>
                         </form>
