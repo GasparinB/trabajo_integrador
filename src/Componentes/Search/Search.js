d33f5d7150eb12f387ee55f15f8c7eaa8df9ec8e
KkathyM <kmartinez@udesa.edu.ar>
2026-09-21 18:59:08 -0300

    Conecté Search con las cards para que se vean al buscar

--- src/Componentes/Search/Search.js
+++ src/Componentes/Search/Search.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Loader from '../Loader/Loader';
+import CardSeries from '../CardSeries/CardSeries';
+import CardPelicula from '../CardPelicula/CardPelicula';
 
 class Search extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             busqueda: '',
             resultados: [],
@@ -12,13 +15,12 @@ class Search extends Component {
     }
 
     componentDidMount() {
-        this.setState({ busqueda: this.props.busqueda });
         const url = `https://api.themoviedb.org/3/search/multi?query=${this.props.busqueda}&page=1`;
         const options = {
             method: 'GET',
             headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjgwNDM1YTlmNmY2ODhjYTI2NGE0YmM3ZmM2NjE4NyIsIm5iZiI6MTc4ODc5MTEyNi4yMjMsInN1YiI6IjZhOWVjOTU2MWRmYjExOWJiZTE0NDRkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fNwSmD2dtb7WrrbApwqjIzh_3rP9QsFLDo4sVwxP8nw'
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjgwNDM1YTlmNmY2ODhjYTI2NGE0YmM3ZmM2NjE4NyIsIm5iZiI6MTc4ODc5MTEyNi4yMjMsInN1YiI6IjZhOWVjOTU2MWRmYjExOWJiZTE0NDRkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fNwSmD2dtb7WrrbApwqjIzh_3rP9QsFLDo4sVwxP8nw'
             }
         };
 
@@ -26,7 +28,8 @@ class Search extends Component {
             .then(response => response.json())
             .then(data => {
                 this.setState({
-                    resultados: data.results || [],
+                    busqueda: this.props.busqueda,
+                    resultados: data.results,
                     cargando: false
                 });
             })
@@ -39,13 +42,56 @@ class Search extends Component {
         }
 
         return (
-            <div>
-                <h2>Resultados de búsqueda</h2>
-                <ul>
-                    {this.state.resultados.map((resultado, index) => (
-                        <li key={index}>{resultado.title || resultado.name}</li>
-                    ))}
-                </ul>
+            <div className="search-results">
+                <h1>Resultados para: {this.state.busqueda}</h1>
+                
+                <section className="grupo-resultados">
+                    <h2>Películas</h2>
+                    <div className="peliculas-container">
+                        {this.state.resultados.map((resultado, index) => {
+                            if (resultado.media_type === 'movie') {
+                                return (
+                                    <CardPelicula
+                                        key={index}
+                                        id={resultado.id}
+                                        title={resultado.title}
+                                        image={`https://image.tmdb.org/t/p/w500${resultado.poster_path}`}
+                                        description={
+                                            resultado.overview !== ''
+                                                ? resultado.overview
+                                                : 'Descripción no disponible.'
+                                        }
+                                    />
+                                );
+                            }
+                            return null;
+                        })}
+                    </div>
+                </section>
+
+                <section className="grupo-resultados">
+                    <h2>Series</h2>
+                    <div className="peliculas-container">
+                        {this.state.resultados.map((resultado, index) => {
+                            if (resultado.media_type === 'tv') {
+                                return (
+                                    <CardSeries
+                                        key={index}
+                                        id={resultado.id}
+                                        title={resultado.name}
+                                        image={`https://image.tmdb.org/t/p/w500${resultado.poster_path}`}
+                                        description={
+                                            resultado.overview !== ''
+                                                ? resultado.overview
+                                                : 'Descripción no disponible.'
+                                        }
+                                    />
+                                );
+                            }
+                            return null;
+                        })}
+                    </div>
+                </section>
             </div>
         );
     }
